7a2ab729a7519e154a25c96444510901e47e0b3d
Edward Tanguay (windows) <edward@tanguay.info>
2025-09-25 08:33:06 +0200

    Remove click

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,24 +2,15 @@
 import { ref } from 'vue';
 import days from '../../parseddata/days.json';
 
-const revealed = ref(Array(days.length).fill(false));
-
-function toggle(index) {
-	revealed.value[index] = !revealed.value[index];
-}
 </script>
 
 <template>
 	<div v-for="(day, idx) in days"
-		 :key="day.suuid"
-		 class="flex flex-col gap-1 p-3 mb-3 w-fit bg-slate-500 rounded">
+		 :key="day.date"
+		 class="flex flex-col gap-1 p-2 mb-3 w-fit bg-slate-500 rounded">
 		<div>
-			<h3 class="font-semibold cursor-pointer"
-				@click="toggle(idx)">
-				{{ day.date }}
-			</h3>
-			<p v-if="revealed[idx]"
-			   class="text-yellow-200">data</p>
+			<h3 class="font-semibold cursor-pointer">{{ day.date }}</h3>
+			<p class="text-yellow-200">data</p>
 		</div>
 	</div>
 </template>
